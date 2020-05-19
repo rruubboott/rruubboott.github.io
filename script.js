a1752fe97b329bc6285c422b09eedc1c883ce69c
rubot <ruairi.murphy@gmail.com>
2020-05-19 15:50:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,6 @@ let regen = document.querySelector("#regen");
 
 regen.style.display = "none";
 
-let pronouns = ["You", "He","She","They","I","Me"];
-
-let actions = ["Are Going To", "Might","Could","Should"];
-
-let event = ["Die","Live","Fall In Love","Live Forever"];
-
 let epiphany = document.querySelector("#epiphany");
 
 btn.addEventListener("click",function(){
